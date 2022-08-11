643dd8b0ee52415651f2f73c13b9460133bdbf40
dzambr13 <dayvuhz@gmail.com>
2022-08-11 13:34:55 -0700

    Product controller complete

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,9 +1,63 @@
-const { Product } = require("../models");
+const { Product } = require('../models')
+const middleware = require('../middleware')
 
 // create product
+const CreateProduct = async (req, res) => {
+  try {
+    const prod = await Product.create({ ...req.body })
+    res.send(prod)
+  } catch (error) {
+    throw error
+  }
+}
 // get one product
-// get all products 
+const OneProduct = async (req, res) => {
+  try {
+    const one = await One.findOne()
+    res.send(one)
+  } catch (error) {
+    throw error
+  }
+}
+// get all products
+const Product = async (req, res) => {
+  try {
+    const items = await Item.findAll()
+    res.send(items)
+  } catch (error) {
+    throw error
+  }
+}
 // update a product
-// delete product 
+const UpdateProduct = async (req, res) => {
+  try {
+    const updateProd = await UpdateProd.create(
+      { ...req.body },
+      { where: { id: req.params.post_id }, returning: true }
+    )
+    res.send(updateProd)
+  } catch (error) {
+    throw error
+  }
+}
+// delete product
+const DeleteProduct = async (req, res) => {
+  try {
+    await Product.destroy({ where: { id: req.params.post } })
+    res.send({
+      msg: 'Product Deleted',
+      payload: req.params.post_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
+}
 
-module.exports = {};
+module.exports = {
+  CreateProduct,
+  Product,
+  OneProduct,
+  UpdateProduct,
+  DeleteProduct
+}

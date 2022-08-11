c0a046ce546ca1fd879292ff1fafb24ab4f5c58d
Leon Glaser <leon.glaser@gmail.com>
2022-08-11 16:36:21 -0400

    create ShowMemberById function/route

--- routes/MemberRouter.js
+++ routes/MemberRouter.js
@@ -3,4 +3,8 @@ const controller = require("../controllers/MemberController");
 
 Router.get("/", controller.ShowAllMembers);
 
+Router.get("/:member_id", controller.ShowMemberById);
+
+Router.post("/newmember", controller.AddNewMember);
+
 module.exports = Router;

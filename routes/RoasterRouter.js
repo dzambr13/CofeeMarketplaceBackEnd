7c3afd3a2f91300355b671ffb5967ce263185a7d
Leon Glaser <leon.glaser@gmail.com>
2022-08-12 10:41:55 -0400

    Protect roaster routes

--- routes/RoasterRouter.js
+++ routes/RoasterRouter.js
@@ -1,17 +1,30 @@
-const Router=require('express').Router()
-const controller=require('../controllers/RoasterController')
-const middleware=require('../middleware')
+const Router = require("express").Router();
+const controller = require("../controllers/RoasterController");
+const middleware = require("../middleware");
 
-Router.get('/find',controller.findARoaster)
-Router.put('/update/:pk',controller.updateRoaster)
-Router.get('/:pk',controller.getOneRoaster)
-Router.get('/',controller.getAllRoasters)
-Router.delete('/delete/:pk',controller.deleteARoaster)
+Router.get("/find", controller.findARoaster);
+Router.put(
+  "/update/:pk",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updateRoaster
+);
+Router.get("/:pk", controller.getOneRoaster);
+Router.get("/", controller.getAllRoasters);
+Router.delete(
+  "/delete/:pk",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.deleteARoaster
+);
 
- 
-Router.post('/login',controller.Login)  
-Router.post('/register',controller.Register) 
-Router.put('/password-update',controller.updatePassword) 
+Router.post("/login", controller.Login);
+Router.post("/register", controller.Register);
+Router.put(
+  "/password-update",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updatePassword
+);
 
-
-module.exports=Router
+module.exports = Router;

a1b5b296909fc18ae4e5c9fd9af7cb349fd1056f
dzambr13 <dayvuhz@gmail.com>
2022-08-12 08:43:01 -0700

    fixed functions for product and review

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -2,26 +2,10 @@ const router = require('express').Router()
 const controller = require('../controllers/ProductController')
 const middleware = require('../middleware')
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f8c6a67c63a47fb0b435ba1802fd6c6dc435e722
 router.get('/', controller.OneProduct)
 router.post('/', controller.CreateProduct) //Add protection
 router.get('/', controller.AllProducts)
-<<<<<<< HEAD
 router.put('/:product_id', controller.UpdateProduct) //Add protection
 router.delete('/:product_id', controller.DeleteProduct) //Add protection
-=======
-router.put('/product/:id', controller.UpdateProduct)
-router.delete('/:product/:id', controller.DeleteProduct)
-
-router.get("/", controller.OneProduct);
-router.get("/", controller.CreateProduct); //Add protection
-router.get("/", controller.AllProducts);
-router.get("/:product_id", controller.UpdateProduct); //Add protection
-router.get("/:product_id", controller.DeleteProduct); //Add protection
-
->>>>>>> f8c6a67c63a47fb0b435ba1802fd6c6dc435e722
 
 module.exports = router

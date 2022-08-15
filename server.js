970087d8dee4a2bb194734710a99534bcfc93429
Leon Glaser <leon.glaser@gmail.com>
2022-08-15 14:46:53 -0400

    install morgan

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
-const app = express()
+const app = express();
 
-const AppRouter = require('./routes/AppRouter')
-const AuthRouter = require('./routes/AuthRouter')
+const AppRouter = require("./routes/AppRouter");
+const AuthRouter = require("./routes/AuthRouter");
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/api',AppRouter)
-app.use('/auth',AuthRouter)
-app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
+app.get("/", (req, res) => res.json({ message: "Server Works" }));
+app.use("/api", AppRouter);
+app.use("/auth", AuthRouter);
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));

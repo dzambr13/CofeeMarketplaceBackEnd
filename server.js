ac7a87a8356a036ae2941780f6fb7fb5d2f370c2
dzambr13 <dayvuhz@gmail.com>
2022-08-15 11:57:30 -0700

    cleanup again

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const logger = require('morgan')
 
 const app = express()
 
@@ -9,10 +10,11 @@ const AuthRouter = require('./routes/AuthRouter')
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
+app.use(logger())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/api',AppRouter)
-app.use('/auth',AuthRouter)
+app.use('/api', AppRouter)
+app.use('/auth', AuthRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
